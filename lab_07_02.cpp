97db45a9807a010738e0089466de5cb17d2a6ea6
Artem Supryhan <suprigan.artem1@gmail.com>
2018-11-16 20:14:06 +0200

    Added code file

---
+++ lab_07_02.cpp
@@ -0,0 +1,53 @@
+// lab_07_02.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <time.h>
+using namespace std;
+double *create_array(int n);
+void output_array(double *arr, int n);
+double max_diff(double *arr, int n);
+double min_diff(double *arr, int n);
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int n;
+	cout << "n = "; cin >> n;
+	double *arr = create_array(n);
+	output_array(arr,n);
+	cout << "Max difference between neighbour elements : " << max_diff(arr, n)<<endl;
+	cout << "Min difference between neighbour elements : " << min_diff(arr, n) << endl;
+	system("pause");
+	return 0;
+}
+double *create_array(int n){
+	srand(time(NULL));
+	double *arr = new double[n];
+	return arr;
+ }
+void output_array(double *arr, int n){
+	cout << "B(n): ";
+	for (int i = 0; i < n; i++){
+		arr[i] = (rand() % 100 - 50) * 0.1;
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
+double max_diff(double *arr, int n){
+	double max_diff = abs(arr[0] - arr[1]);
+	for (int j = 2; j < n; j++){
+		if (abs(arr[j] - arr[j - 1])>max_diff){
+			max_diff = abs(arr[j] - arr[j - 1]);
+		}
+	}
+	return max_diff;
+}
+double min_diff(double *arr, int n){
+	double min_diff = abs(arr[0] - arr[1]);
+	for (int g = 2; g < n; g++){
+		if (abs(arr[g] - arr[g - 1]) < min_diff){
+			min_diff = abs(arr[g] - arr[g - 1]);
+		}
+	}
+	return min_diff;
+}
