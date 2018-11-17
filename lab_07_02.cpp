67c8a31121200b0f795175f3c3a04552a1d21834
Artem Supryhan <suprigan.artem1@gmail.com>
2018-11-17 21:52:23 +0200

    Corrected code style

--- lab_07_02.cpp
+++ lab_07_02.cpp
@@ -1,52 +1,78 @@
-// lab_07_02.cpp: îïðåäåëÿåò òî÷êó âõîäà äëÿ êîíñîëüíîãî ïðèëîæåíèÿ.
-//
-
 #include "stdafx.h"
 #include <iostream>
-#include <time.h>
+#include <ctime>
 using namespace std;
-double *create_array(int n);
-void output_array(double *arr, int n);
-double max_diff(double *arr, int n);
-double min_diff(double *arr, int n);
+
+#define ARRAY_RANGE (100 - 50)*0.1 
+
+float *createArray(int n);
+void initializeArray(float *arr, int n);
+void outputArray(float *arr, int n);
+float getMaxNeighbourDifference(float *arr, int n);
+float getMinNeighbourDifference(float *arr, int n);
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int n;
-	cout << "n = "; cin >> n;
-	double *arr = create_array(n);
-	output_array(arr,n);
-	cout << "Max difference between neighbour elements : " << max_diff(arr, n)<<endl;
-	cout << "Min difference between neighbour elements : " << min_diff(arr, n) << endl;
+	bool integer = false;
+	float n;
+	while (integer == false){
+		cout << "Insert a size of the array :  "; cin >> n;
+		if (n == int(n) && n > 0){
+			integer = true;
+		}
+		else {
+			cout << "Error. Natural number expected." << endl;
+		}
+	}
+	float *arr = createArray(n);
+	initializeArray(arr, n);
+	outputArray(arr,n);
+	if (n > 1){
+		cout << "Max difference between neighbour elements : " << getMaxNeighbourDifference(arr, n) << endl;
+		cout << "Min difference between neighbour elements : " << getMinNeighbourDifference(arr, n) << endl;
+	}
+	else {
+		cout << "There are no neightbour elements." << endl;
+	}
 	system("pause");
 	return 0;
 }
-double *create_array(int n){
-	double *arr = new double[n];
+
+float *createArray(int n){
+	srand(time(0));
+	float *arr = new float[n];
 	return arr;
- }
-void output_array(double *arr, int n){
-	srand(time(NULL));
-	cout << "B(n): ";
+}
+
+void initializeArray(float *arr, int n){
 	for (int i = 0; i < n; i++){
-		arr[i] = (rand() % 100 - 50) * 0.1;
+		arr[i] = rand() % ARRAY_RANGE;
+	}
+}
+
+void outputArray(float *arr, int n){
+	cout << "Your array B(n): ";
+	for (int i = 0; i < n; i++){
 		cout << arr[i] << " ";
 	}
 	cout << endl;
 }
-double max_diff(double *arr, int n){
-	double max_diff = abs(arr[0] - arr[1]);
-	for (int j = 2; j < n; j++){
-		if (abs(arr[j] - arr[j - 1])>max_diff){
-			max_diff = abs(arr[j] - arr[j - 1]);
+
+float getMaxNeighbourDifference(float *arr, int n){
+	float max_diff = 0;
+	for (int i = 1; i < n; i++){
+		if (fabs(arr[i] - arr[i - 1]) > max_diff){
+			max_diff = fabs(arr[i] - arr[i - 1]);
 		}
 	}
 	return max_diff;
 }
-double min_diff(double *arr, int n){
-	double min_diff = abs(arr[0] - arr[1]);
-	for (int g = 2; g < n; g++){
-		if (abs(arr[g] - arr[g - 1]) < min_diff){
-			min_diff = abs(arr[g] - arr[g - 1]);
+
+float getMinNeighbourDifference(float *arr, int n){
+	float min_diff = fabs(arr[0] - arr[1]);
+	for (int i = 1; i < n; i++){
+		if (fabs(arr[i] - arr[i - 1]) < min_diff){
+			min_diff = fabs(arr[i] - arr[i - 1]);
 		}
 	}
 	return min_diff;
